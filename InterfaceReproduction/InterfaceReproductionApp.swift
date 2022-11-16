e5b43c13ded9e1baecb2ba11b0d8b18864dd891c
Jack Dellamea <jdellamea@lcs.on.ca>
2022-11-16 11:33:28 -0500

    Initial Commit

---
+++ InterfaceReproduction/InterfaceReproductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceReproductionApp.swift
+//  InterfaceReproduction
+//
+//  Created by Jack Dellamea on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceReproductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
